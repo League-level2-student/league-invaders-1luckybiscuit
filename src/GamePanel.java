a4b2ad36d3ae468eacd7ae096792d62393cce0c3
League Student <league@WTS-iMac-11.attlocal.net>
2019-06-09 17:29:58 -0700

    League Invaders on Part 2
    We bout to get the bread by invading the league bois

---
+++ src/GamePanel.java
@@ -0,0 +1,20 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class GamePanel extends JPanel implements ActionListener {
+	Timer time;
+	GamePanel() {
+		time = new Timer(1000/60,this);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("yeetus feetus");
+	}
+	void startGame() {
+		time.start();
+	}
+}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,25 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	final int WIDTH = 500;
+	final int HEIGHT = 800;
+	GamePanel gamePanel;
+	public static void main(String[] args) {
+		LeagueInvaders li = new LeagueInvaders();
+		li.setup();
+	}
+	LeagueInvaders() {
+		frame = new JFrame();
+	gamePanel = new GamePanel();
+	}
+	void setup() {
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+	}
 }
+
